b964baed50cf976e869e8e8a88358675a3b12a7e
Jake Ludington <durzaskillz@gmail.com>
2017-02-02 19:46:03 -0500

    Update generic_RPG.js

--- generic_RPG.js
+++ generic_RPG.js
@@ -1,1 +1,26 @@
 var game = new Phaser.Game(1500, 750)
+class Boot {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+game.state.add("Boot",Boot);
+game.state.start("Boot");
