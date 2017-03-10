b8afbfa757c3460e1d3c27150a069b78a826e69f
Jake Ludington <durzaskillz@gmail.com>
2017-03-09 21:02:42 -0500

    Update generic_RPG.js

--- generic_RPG.js
+++ generic_RPG.js
@@ -5,9 +5,18 @@ class Boot {
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
   }
+ create() {
+    this.state.start("Load")
+  }
 }
 
-
+class Load {
+  preload() {
+    console.log("Loading");
+    this.load.image("bg",C.bg.file)
+    this.load.spritesheet("slime1", "assets/Slime_enemy_1_game.png", 32, 32, 2);
+    this.load.spritesheet("poison_potion", "assets/poison_potion.png", 25, 25, 7);
+  }
 
 
 
